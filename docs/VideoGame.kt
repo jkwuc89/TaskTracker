29378218652b892ce92265bad0e5807dfdcf866f
Keith Wedinger <keith.wedinger@improving.com>
2018-04-05 15:13:09 -0400

    Added presentation code samples

---
+++ docs/VideoGame.kt
@@ -0,0 +1,27 @@
+// val creates getter only, var creates getter and setter
+data class VideoGame(val name: String, val publisher: String, var reviewScore: Int)
+
+fun main(args : Array<String>) {
+    // Supports property access
+    val aVideoGame = VideoGame("Call of Duy", "Activision", 90)
+    println("Video game name = ${aVideoGame.name}") // String interpolation - neat!
+
+    // Named function arguments
+    val anotherVideoGame: VideoGame(publisher = "Microsoft", name = "Halo", reviewScore = 95)
+
+    // Supports deconstructing properties as follows:
+    val listOfGame: List<VideoGame> = listOf(game, betterGame)
+    for ((gameName, gamePublisher, gameScore) in listOfGame) {
+        // Do something with the individual properties
+    }
+
+    // Other interesting Kotlin tidbits
+
+    // Type inference:
+    val aString = "String" // inferred as String
+
+    // Nullable, only references personalInfo if client not null and email if
+    // client.personalInfo is not null
+    val email = client?.personalInfo?.email
+}
+
