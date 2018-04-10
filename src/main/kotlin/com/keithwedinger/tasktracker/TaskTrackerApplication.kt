8fb7161f3aef2fe5bd8af9201c0342a73fe4a7bd
Keith Wedinger <keith.wedinger@improving.com>
2018-04-10 17:40:22 -0400

    Cleaning up main app

--- src/main/kotlin/com/keithwedinger/tasktracker/TaskTrackerApplication.kt
+++ src/main/kotlin/com/keithwedinger/tasktracker/TaskTrackerApplication.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.domain.AuditorAware
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 /**
